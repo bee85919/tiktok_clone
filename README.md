a3c8d4011a55b7ab126a229a27811b82eca9ff14
bee <bee85919@gmail.com>
2023-12-11 22:02:57 +0900

    7.3 Video Player

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-### 3.2 Project files
-- UI 확인할 때 mobbin 이라는 사이트를 참고하면 좋다.
+# tiktok_clone
 
+A new Flutter project.
 
-### 4.3 Sign Up Form
-- method나 프로퍼티 이름 앞에 _를 달면 private이 된다.
-    - 위젯의 생명 주기에 관련된 메서드들은 앞에 _가 없는 것을 볼 수 있다.
+## Getting Started
 
-- code challenge: sign_up_screen.dart
-    - "Use email & password" 버튼을 GestureDetector로 감싸주었다. 유저의 버튼 클릭을 감지하기 위해서다. 여기에 GestureDetector를 넣는 대신에 AuthButton에 함수를 전달할 수 있게 만들고 이 FractionallySizedBox를 GestureDetector로 감싸는 방법을 구현해 볼 수 있을까?
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
--- lib/features/authentication/widgets/auth_button.dart
+++ lib/features/authentication/widgets/auth_button.dart
@@ -5,46 +5,41 @@ import 'package:tiktok_clone/constants/sizes.dart';
 class AuthButton extends StatelessWidget {
   final String text;
   final FaIcon icon;
-  final VoidCallback? onTap;
 
   const AuthButton({
     super.key,
     required this.text,
     required this.icon,
-    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: FractionallySizedBox(
-        widthFactor: 1,
-        child: Container(
-          padding: const EdgeInsets.all(Sizes.size14),
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.grey.shade300,
-              width: Sizes.size1,
+    return FractionallySizedBox(
+      widthFactor: 1,
+      child: Container(
+        padding: const EdgeInsets.all(Sizes.size14),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.grey.shade300,
+            width: Sizes.size1,
+          ),
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: icon,
             ),
-          ),
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: icon,
+            Text(
+              text,
+              style: const TextStyle(
+                fontSize: Sizes.size16,
+                fontWeight: FontWeight.w600,
               ),
-              Text(
-                text,
-                style: const TextStyle(
-                  fontSize: Sizes.size16,
-                  fontWeight: FontWeight.w600,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ],
-          ),
+              textAlign: TextAlign.center,
+            ),
+          ],
         ),
       ),
     );
